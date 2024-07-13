71cb5624bb5731471747c77ab0d95e32c652adff
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-07-13 13:01:02 +0200

    made a display home page

--- spotify-clone/src/components/NavBar.jsx
+++ spotify-clone/src/components/NavBar.jsx
@@ -2,32 +2,39 @@ import { assets } from "../assets/assets";
 
 const NavBar = () => {
   return (
-    <div className="w-full flex justify-between items-center font-semibold">
-      <div className="flex items-center gap-2">
-        <img
-          className="w-8 bg-black p-2 rounded-2x1 cursor-pointer"
-          src={assets.arrow_left}
-          alt=""
-        />
-        <img
-          className="w-8 bg-black p-2 rounded-2x1 cursor-pointer"
-          src={assets.arrow_right}
-          alt=""
-        />
-      </div>
+    <>
+      <div className="w-full flex justify-between items-center font-semibold">
+        <div className="flex items-center gap-2">
+          <img
+            className="w-8 bg-black p-2 rounded-2x1 cursor-pointer"
+            src={assets.arrow_left}
+            alt=""
+          />
+          <img
+            className="w-8 bg-black p-2 rounded-2x1 cursor-pointer"
+            src={assets.arrow_right}
+            alt=""
+          />
+        </div>
 
-      <div className="flex items-center gap-4 ">
-        <p className="bg-white text-black text-[15px] px-4 py-1 hidden md:block cursor-pointer border border-black rounded-lg">
-          Explore Premium
-        </p>
-        <p className="bg-black text-white text-[15px] px-4 py-1 hidden md:block cursor-pointer border border-black rounded-lg">
-          Install App
-        </p>
-        <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">
-          S
-        </p>
+        <div className="flex items-center gap-4 ">
+          <p className="bg-white text-black text-[15px] px-4 py-1 hidden md:block cursor-pointer border border-black rounded-2xl">
+            Explore Premium
+          </p>
+          <p className="bg-black text-white text-[15px] px-4 py-1 hidden md:block cursor-pointer border border-black rounded-2xl">
+            Install App
+          </p>
+          <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">
+            S
+          </p>
+        </div>
       </div>
-    </div>
+      <div className="flex items-center gap-2 mt-4 ">
+        <p className="bg-white text-black px-4 py-1  rounded-2xl cursor-pointer">All</p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Podcasts</p>
+      </div>
+    </>
   );
 };
 
