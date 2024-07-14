2380972d28670fb2ed53ceb246344604ca63453a
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-07-14 11:39:14 +0200

    add logic to prev and next buttons

--- spotify-clone/src/components/NavBar.jsx
+++ spotify-clone/src/components/NavBar.jsx
@@ -1,16 +1,20 @@
 import { assets } from "../assets/assets";
-
+import { useNavigate } from "react-router-dom";
 const NavBar = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
           <img
+            onClick={() => navigate(-1)}
             className="w-8 bg-black p-2 rounded-2x1 cursor-pointer"
             src={assets.arrow_left}
             alt=""
           />
           <img
+            onClick={() => navigate(+1)}
             className="w-8 bg-black p-2 rounded-2x1 cursor-pointer"
             src={assets.arrow_right}
             alt=""
@@ -30,9 +34,13 @@ const NavBar = () => {
         </div>
       </div>
       <div className="flex items-center gap-2 mt-4 ">
-        <p className="bg-white text-black px-4 py-1  rounded-2xl cursor-pointer">All</p>
+        <p className="bg-white text-black px-4 py-1  rounded-2xl cursor-pointer">
+          All
+        </p>
         <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
-        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Podcasts</p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">
+          Podcasts
+        </p>
       </div>
     </>
   );
