2356f8871cc3d72890591278057f51b8c7922169
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-07-14 14:20:04 +0200

    seekBar done

--- spotify-clone/src/context/playerContext.jsx
+++ spotify-clone/src/context/playerContext.jsx
@@ -39,6 +39,10 @@ const PlayerContextProvider = (props) => {
   useEffect(() => {
     setTimeout(() => {
       audioRef.current.ontimeupdate = () => {
+        seekBar.current.style.width =
+          Math.floor(
+            (audioRef.current.currentTime / audioRef.current.duration) * 100
+          ) + "%";
         // Ažurira vreme reprodukcije pesme
         setTime({
           currentTime: {
